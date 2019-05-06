ce3fc520754e7b73639ae9ed4df352514dec739e
jacobcolbert <jacob.colbert@gmail.com>
2019-05-06 12:57:06 -0400

    Make Market History independently collapsible

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    
     if(details['url']&&details['url'].match(/dashboard/)) {
       chrome.tabs.executeScript(null,{file:"dashboard.js"});
       chrome.tabs.insertCSS(null,{file:"dashboard.css"});
+    }
+    
+    if(details['url']&&details['url'].match(/markets/)) {
+      chrome.tabs.executeScript(null,{file:"market.js"});
+      chrome.tabs.insertCSS(null,{file:"market.css"});
     }
 }); 
